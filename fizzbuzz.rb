712fd0ba15cbb56c62accc776cbedae25695be98
Irina Bright <missirinabright@gmail.com>
2019-05-16 00:57:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 === 0 
     puts "Fizz"
     
-  elsif int % 5 == 0 
+  elsif int % 5 === 0 
     puts "Buzz"
     
-  elsif int % 3 == 0 and int % 5 == 0
+  elsif int % 3 === 0 and int % 5 === 0
     puts "FizzBuzz"
   
     else
